6b84c2a9e0c05910c941f43c38a98e278477d9ad
ININDC00614 <ININDC00614@DESKTOP-PSK9T3G.azentio.com>
2023-08-25 18:48:39 +0530

    Code only commited on 25/08/2023

--- src/test/java/Runner/TestApplication.java
+++ src/test/java/Runner/TestApplication.java
@@ -15,9 +15,11 @@ import utilities.ExtentTestManager;
                            "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                            "rerun:ReRunScenarios/FailedReRun.txt"}
               // ,dryRun = false //to check compilation errors                                      
-              , tags="@AT_051"
+              , tags="@AT_048"
 
 )
+
+//check
 
 // P@th123$%@z
 
